a47fe559d1478c13d43c1a8146fdc628ac08e859
MohammadFahad2606 <design4youonline@gmail.com>
2024-10-29 20:40:46 +0500

    Sing Up page

--- src/Modules/Pages/Auth/Singup.jsx
+++ src/Modules/Pages/Auth/Singup.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Singup = () => {
   return (
+   
+    <h1 className='text-3xl !bg-amber-500'>Sing Up</h1>
     
-    <h1 className="text-3xl text-center font-bold underline">
-    Hello world!
-  </h1>
+  
      
     
   )
--- src/app/(pages)/auth/singin/page.jsx
+++ src/app/(pages)/auth/singin/page.jsx
@@ -3,9 +3,20 @@ import Singup from '@/Modules/Pages/Auth/Singup'
 
 const singin = () => {
   return (
-    <div>
-      <Singup/>
-    </div>
+    
+    <div className= 'w-screen h-screen flex  flex-col items-center justify-center'>
+        {/* <Singup/> */}
+        <div className="flex flex-col w-[50%] gap-5 font-bold">
+
+    <h1 className='text-center text-3xl p-2 '>Sing Up</h1>
+    <input placeholder='Name' className=' bg-gray-100 shadow-md rounded-full p-5 outline-none border-none' type="text" />
+    <input placeholder='Email' className='bg-gray-100 shadow-md rounded-full p-5 outline-none border-none' type="email" />
+    <input placeholder='Password' className='bg-gray-100 shadow-md rounded-full p-5 outline-none border-none' type="password" />
+    <button className='bg-slate-600 rounded-full p-5 text-white'>SingUp</button>
+   
+        </div>
+   </div>
+   
   )
 }
 
